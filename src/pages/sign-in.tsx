1454a5205a3a1624c264a013e613f62572336759
jihanlugas <jihanlugas2@gmail.com>
2021-03-03 22:14:20 +0700

    api

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -47,7 +47,7 @@ const Signin: NextPage<Props> = ({ }) => {
                                 return (
                                     <Form>
                                         <div className={"text-xl flex justify-center"}>
-                                            Sign Inn
+                                            Sign In
                                             </div>
                                         <div className="mb-4">
                                             <TextField
