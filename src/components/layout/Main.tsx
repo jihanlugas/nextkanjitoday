48b6ab2b3d29ad0ac0a8e9ce2a6237d65762e6b2
jihanlugas <jihanlugas2@gmail.com>
2021-03-03 21:51:29 +0700

    api

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,9 +1,15 @@
 import { Fragment } from 'react'
+import Head from "next/head"
 
-const Main = ({ children } : { children: React.ReactNode}) => {
+const Main = ({ children }: { children: React.ReactNode }) => {
     return (
+
         <Fragment>
+            <Head>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+            </Head>
             <div className="app flex min-h-screen bg-gray-200 w-full">
+
                 {children}
             </div>
         </Fragment>
